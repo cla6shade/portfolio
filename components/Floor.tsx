1ab90d37377422afa124717498f7810c817031b5
cla6shade <cla6shade@github.com>
2025-11-12 17:12:10 +0900

    design: adjust floor position

--- components/Floor.tsx
+++ components/Floor.tsx
@@ -21,7 +21,7 @@ export default function Floor() {
   });
 
   return (
-    <mesh rotation={[-Math.PI / 2, 0, 0]} position={[0, -5, 0]} receiveShadow>
+    <mesh rotation={[-Math.PI / 2, 0, 0]} position={[0, 0, 0]} receiveShadow={true}>
       <planeGeometry args={[width, height]} />
       <meshStandardMaterial
         map={diffuse}
