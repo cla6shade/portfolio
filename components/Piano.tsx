04882a80886014dd24b99ca0f92d3e9530d11999
cla6shade <cla6shade@github.com>
2025-11-12 17:11:14 +0900

    feat: make rotation modifiable and adjust black key height

--- components/Piano.tsx
+++ components/Piano.tsx
@@ -4,16 +4,17 @@ import XStand from './XStand';
 
 interface PianoProps {
   offset?: [number, number, number];
+  rotation?: [number, number, number];
 }
 
-export default function Piano({ offset = [0, 0, 0] }: PianoProps) {
+export default function Piano({ offset = [6, 4, 0], rotation = [0, -Math.PI / 3, 0] }: PianoProps) {
   const keys = [];
   const whiteKeyWidth = 0.15;
   const whiteKeyLength = 1.0;
   const whiteKeyHeight = 0.1;
   const blackKeyWidth = 0.1;
   const blackKeyLength = 0.6;
-  const blackKeyHeight = 0.15;
+  const blackKeyHeight = 0.075;
 
   const whiteKeyPattern = [0, 2, 4, 5, 7, 9, 11]; // C, D, E, F, G, A, B
   const blackKeyPattern = [1, 3, 6, 8, 10]; // C#, D#, F#, G#, A#
@@ -57,7 +58,7 @@ export default function Piano({ offset = [0, 0, 0] }: PianoProps) {
       keys.push(
         <mesh
           key={`black-${i}`}
-          position={[blackKeyX, blackKeyHeight / 2 + whiteKeyHeight / 2, -whiteKeyLength / 2 + blackKeyLength / 2]}
+          position={[blackKeyX, blackKeyHeight / 2 + whiteKeyHeight, -whiteKeyLength / 2 + blackKeyLength / 2]}
           castShadow
           receiveShadow
         >
@@ -76,20 +77,17 @@ export default function Piano({ offset = [0, 0, 0] }: PianoProps) {
   const caseDepth = whiteKeyLength + 0.3;
 
   return (
-    <group position={offset} rotation={[0, -Math.PI / 6, 0]}>
-      {/* 피아노 본체 (케이스) - 건반보다 크게 */}
+    <group position={offset} rotation={rotation}>
       <mesh position={[0, -0.2, 0]} castShadow receiveShadow>
         <boxGeometry args={[caseWidth, 0.3, caseDepth]} />
         <meshStandardMaterial color="#222222" />
       </mesh>
 
-      {/* 건반들 */}
       <group position={[-pianoWidth / 2, 0, 0]}>
         {keys}
       </group>
 
-      {/* X다리 스탠드 */}
-      <XStand width={caseWidth} yPosition={-2.175} />
+      <XStand width={caseWidth} yPosition={-2} />
     </group>
   );
 }
