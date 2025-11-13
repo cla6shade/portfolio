f2599151e26375b9c3403209d5ed0edae2b7388c
cla6shade <cla6shade@gmail.com>
2025-11-13 18:29:17 +0900

    feat: 히어로 섹션 구현

--- components/Scene.tsx
+++ components/Scene.tsx
@@ -1,44 +1,52 @@
 'use client'
 
-import { Canvas, ThreeEvent } from '@react-three/fiber';
-import Piano from '@/components/Piano';
+import { Canvas, ThreeEvent  } from '@react-three/fiber';
 import Floor from '@/components/Floor';
 import StageLight from '@/components/StageLight';
-// import { OrbitControls } from '@react-three/drei';
-// import CameraLogger from '@/components/CameraLogger';
-
-function PianoScene() {
-
-  const handleGroupClick = (e: ThreeEvent<MouseEvent>) => {
-    e.stopPropagation();
-  };
-
-  return (
-    <>
-      <ambientLight intensity={0.3} />
-      <StageLight position={[8, 13, 1]} target={[8, 4, 1]} />
-      <Floor />
-      <group onClick={handleGroupClick}>
-        <Piano offset={[8, 4, 2]} rotation={[0, - Math.PI / 3, 0]} />
-      </group>
-    </>
-  );
-}
+import { useHero } from '@/features/hero/HeroProvider';
+import CameraController from '@/components/scene/CameraController';
+import Piano from '@/components/piano/Piano';
 
 export default function Scene() {
+  const {
+    isPianoFocused,
+    cameraTarget,
+    lookAtTarget,
+    isAnimating,
+    setIsAnimating,
+    focusPiano,
+  } = useHero();
+
+  const handleGroupClick = (e: ThreeEvent<MouseEvent>) => {
+    if (!isPianoFocused) {
+      e.stopPropagation();
+      focusPiano();
+    }
+  };
+
+  const isSoundEnabled = isPianoFocused && !isAnimating;
   return (
-    <Canvas camera={{
-      fov: 75,
-      position: [
-        -0.68,
-        9.19,
-        14.46
-      ]
-    }} shadows
-    >
-      {/*<OrbitControls makeDefault target={[0, -5, -10]} />*/}
-      {/*<CameraLogger />*/}
-      <PianoScene />
-    </Canvas>
+    <div className="w-full h-full flex items-center justify-center">
+      <div className="w-full max-h-full aspect-video">
+        <Canvas camera={{
+          fov: 75,
+          position: [-0.68, 9.19, 14.46]
+        }} shadows
+        >
+          <CameraController
+            targetPosition={cameraTarget}
+            targetLookAt={lookAtTarget}
+            isAnimating={isAnimating}
+            setIsAnimating={setIsAnimating}
+          />
+          <ambientLight intensity={0.1} />
+          <StageLight position={[8, 13, 1]} target={[8, 4, 1]} />
+          <Floor />
+          <group onClick={handleGroupClick}>
+            <Piano isSoundEnabled={isSoundEnabled} />
+          </group>
+        </Canvas>
+      </div>
+    </div>
   )
 }
