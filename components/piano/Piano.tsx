0b4c79977564f41eacb16b7de3b881a86e53bbb9
cla6shade <cla6shade@gmail.com>
2025-11-13 18:28:39 +0900

    feat: 사운드 로드를 위해 피아노 모델 래퍼 작성

---
+++ components/piano/Piano.tsx
@@ -0,0 +1,24 @@
+import { Suspense, use, useMemo } from 'react';
+import { loadPiano } from '@/components/piano/pianoSound';
+import PianoModel from '@/components/piano/PianoModel';
+
+export type PianoProps = {
+  isSoundEnabled: boolean;
+}
+export default function Piano({ isSoundEnabled }: PianoProps) {
+  return (
+    isSoundEnabled ? (
+      <Suspense fallback={<PianoModel />}>
+        <SoundBoundPiano />
+      </Suspense>
+    ) : (
+      <PianoModel />
+    )
+  )
+}
+
+function SoundBoundPiano() {
+  const pianoPromise = useMemo(() => loadPiano(), []);
+  const player = use(pianoPromise);
+  return <PianoModel player={player} />;
+}
