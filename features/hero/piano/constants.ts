eeaaede851df5b61a611e6a4afdc2272156b50dc
cla6shade <cla6shade@github.com>
2025-11-17 09:55:51 +0900

    design: modify press depth

--- features/hero/piano/constants.ts
+++ features/hero/piano/constants.ts
@@ -3,13 +3,13 @@ export const KEY_PROPERTIES = {
     width: 0.1,
     height: 0.075,
     length: 0.6,
-    pressDepth: 0.015,
+    pressDepth: 0.025,
   },
   white: {
     width: 0.15,
     height: 0.1,
     length: 1.0,
-    pressDepth: 0.02,
+    pressDepth: 0.05,
   },
 };
 export const TOTAL_KEYS = 88;
@@ -22,3 +22,7 @@ export const CASE_LENGTH = KEY_PROPERTIES.white.length + 0.3;
 
 export const whiteKeyPattern = [0, 2, 4, 5, 7, 9, 11]; // C, D, E, F, G, A, B
 export const blackKeyPattern = [1, 3, 6, 8, 10]; // C#, D#, F#, G#, A#
+
+export const PIANO_POSITION: [number, number, number] = [8, 4, 1];
+export const PIANO_ROTATION: [number, number, number] = [0, -Math.PI / 3, 0];
+export const PIANO_SIZE: [number, number, number] = [CASE_WIDTH, 0.3, CASE_LENGTH];
