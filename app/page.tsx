02fe676123e2da4a160d135c7566b0c24d4ee8b2
cla6shade <cla6shade@gmail.com>
2025-11-18 23:12:19 +0900

    feat: add projects section

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,11 @@
 import HeroSection from '@/features/hero/HeroSection';
+import ProjectsSection from '@/features/projects/ProjectsSection';
 
 export default function Home() {
   return (
     <>
       <HeroSection />
+      <ProjectsSection />
     </>
   );
 }
