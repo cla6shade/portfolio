489937a29450182ac4ce30342033f6b2bf7ac8d5
cla6shade <cla6shade@gmail.com>
2025-11-18 19:50:54 +0900

    design: 네비게이션 텍스트 간격 조정

--- components/Navigation.tsx
+++ components/Navigation.tsx
@@ -3,7 +3,7 @@
 export default function Navigation() {
   return (
     <nav className="fixed top-0 left-0 right-0 z-50 w-full">
-      <div className="px-20 py-4 shadow-lg">
+      <div className="px-20 lg:px-30 xl:px-40 py-4 shadow-lg">
         <div className="flex items-center justify-between">
           <div className="text-xl font-playwrite font-thin">Cla6shade</div>
           <div className="flex gap-8 items-center">
