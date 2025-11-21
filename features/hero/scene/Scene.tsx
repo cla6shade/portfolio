b5cc218147e266d325953953e1d261dabcd8e855
cla6shade <cla6shade@github.com>
2025-11-21 11:19:58 +0900

    refactor: camera ref를 context의 camera ref로 전달

--- features/hero/scene/Scene.tsx
+++ features/hero/scene/Scene.tsx
@@ -4,15 +4,13 @@ import { Canvas, ThreeEvent } from '@react-three/fiber';
 import Floor from '@/features/hero/scene/Floor';
 import StageLight from '@/features/hero/scene/StageLight';
 import { useHero } from '@/features/hero/HeroProvider';
-import CameraController from '@/features/hero/scene/CameraController';
 import Piano from '@/features/hero/piano/Piano';
 
 import { useRef } from 'react';
 import { INITIAL_CAMERA_FOV, INITIAL_CAMERA_POSITION } from '@/features/hero/piano/constants';
 
 export default function Scene() {
-  const { isPianoFocused, cameraTarget, lookAtTarget, isAnimating, setIsAnimating, focusPiano } =
-    useHero();
+  const { isPianoFocused, isAnimating, focusPiano, setCameraRef } = useHero();
   const canvasRef = useRef<HTMLDivElement>(null);
 
   const handleGroupClick = (e: ThreeEvent<MouseEvent>) => {
@@ -32,13 +30,8 @@ export default function Scene() {
             position: INITIAL_CAMERA_POSITION,
           }}
           shadows
+          onCreated={({ camera }) => setCameraRef(camera)}
         >
-          <CameraController
-            targetPosition={cameraTarget}
-            targetLookAt={lookAtTarget}
-            isAnimating={isAnimating}
-            setIsAnimating={setIsAnimating}
-          />
           <StageLight position={[7, 13, 0]} target={[7, 4, 0]} />
           <Floor />
           <group onClick={handleGroupClick}>
