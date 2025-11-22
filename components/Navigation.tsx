c36480b2eea634084cc96b33cd0dea366e46bc99
cla6shade <cla6shade@github.com>
2025-11-22 17:16:14 +0900

    design: hover bg 추가

--- components/Navigation.tsx
+++ components/Navigation.tsx
@@ -30,10 +30,10 @@ export default function Navigation() {
             <NavigationMenu>
               <NavigationMenuList>
                 {navigationItems.map((item) => (
-                  <NavigationMenuItem key={item.href}>
+                  <NavigationMenuItem key={`navigation-item-${item.href}`}>
                     <NavigationMenuLink
                       href={item.href}
-                      className="group inline-flex h-10 w-max items-center justify-center rounded-md bg-transparent px-4 py-2 text-sm font-medium transition-colors hover:bg-accent hover:text-accent-foreground focus:bg-accent focus:text-accent-foreground focus:outline-none disabled:pointer-events-none disabled:opacity-50"
+                      className="px-4 hover:bg-[rgba(255,255,255,.1)]"
                     >
                       {item.label}
                     </NavigationMenuLink>
