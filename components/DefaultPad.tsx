84a7146c89a4997dc248a737c8d5c8aeaa5df094
cla6shade <cla6shade@github.com>
2025-11-24 10:15:23 +0900

    design: 더 큰 기기에서 px 조정

--- components/DefaultPad.tsx
+++ components/DefaultPad.tsx
@@ -6,5 +6,5 @@ interface DefaultPadProps {
 }
 
 export default function DefaultPad({ children, className = '' }: DefaultPadProps) {
-  return <div className={`px-16 md:px-20 lg:px-60 ${className}`}>{children}</div>;
+  return <div className={`px-16 md:px-20 lg:px-40 xl:px-60 ${className}`}>{children}</div>;
 }
