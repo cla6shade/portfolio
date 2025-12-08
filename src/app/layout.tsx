d5f04f65a97e6e44ac0aa43f33941f3ecf35d593
cla6shade <cla6shade@github.com>
2025-12-08 15:11:33 +0900

    chore: add analytics and speed insights

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,9 +1,12 @@
 import type { Metadata } from 'next';
 import { Noto_Sans, Playwrite_US_Trad, Roboto } from 'next/font/google';
+import { SpeedInsights } from '@vercel/speed-insights/next';
+import { Analytics } from '@vercel/analytics/next';
 import './globals.css';
 import { ReactNode } from 'react';
 import Navigation from '@/components/Navigation';
 import fetchFollowers from '@/utils/fetch-followers';
+
 const notoSans = Noto_Sans({
   variable: '--font-noto-sans',
   subsets: ['latin'],
@@ -18,8 +21,8 @@ const playWrite = Playwrite_US_Trad({
 });
 
 export const metadata: Metadata = {
-  title: 'CLA6SHADE',
-  description: '',
+  title: 'CLAVISHADE',
+  description: '음악과 성취를 좋아하는 프론트엔드 엔지니어입니다.',
 };
 
 export default function RootLayout({
@@ -34,6 +37,8 @@ export default function RootLayout({
       >
         <Navigation followersPromise={fetchFollowers()} />
         {children}
+        <Analytics />
+        <SpeedInsights />
       </body>
     </html>
   );
