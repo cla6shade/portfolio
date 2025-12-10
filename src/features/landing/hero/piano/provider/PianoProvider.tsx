151bb59a9fa857cdad378df5a691679be00ca91c
cla6shade <cla6shade@gmail.com>
2025-12-10 17:31:27 +0900

    feat: midi player를 context에서 제공

--- src/features/landing/hero/piano/provider/PianoProvider.tsx
+++ src/features/landing/hero/piano/provider/PianoProvider.tsx
@@ -1,13 +1,16 @@
-import { createContext, ReactNode, use } from 'react';
+import { createContext, ReactNode, use, useMemo } from 'react';
 import { SplendidGrandPiano } from 'smplr';
 import { loadPianoSound } from '@/features/landing/hero/piano/soundfont/pianoSound';
+import Player from '@/features/landing/hero/piano/midi/Player';
 
 interface PianoContextValue {
   soundfont: SplendidGrandPiano | null;
+  midiPlayer: Player | null;
 }
 
 export const PianoContext = createContext<PianoContextValue>({
   soundfont: null,
+  midiPlayer: null,
 });
 
 interface PianoContextProps {
@@ -16,10 +19,12 @@ interface PianoContextProps {
 
 export default function PianoProvider({ children }: PianoContextProps) {
   const soundfont = use(loadPianoSound());
+  const midiPlayer = useMemo(() => new Player(), []);
   return (
     <PianoContext.Provider
       value={{
         soundfont,
+        midiPlayer,
       }}
     >
       {children}
