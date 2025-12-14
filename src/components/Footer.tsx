9249401ecdafaa1bd86c4a0286b1f89e8066e16f
cla6shade <cla6shade@github.com>
2025-12-14 14:54:07 +0900

    design: 다크 -> 라이트 모드 전환 시 푸터 색상이 그대로 유지되던 문제점 해결

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -2,11 +2,9 @@ import Flex from '@/components/container/Flex';
 
 export default function Footer() {
   return (
-    <footer className="bg-black py-8">
+    <footer className="bg-background py-8 border border-t-border">
       <Flex justify="center" align="center">
-        <p className="text-sm text-gray-500">
-          &copy; 2025 Cla6shade. All rights reserved.
-        </p>
+        <p className="text-sm text-gray-500">&copy; 2025 Cla6shade. All rights reserved.</p>
       </Flex>
     </footer>
   );
