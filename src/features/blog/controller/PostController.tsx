13db62173c2ea9caaaeee9068e0aa76f61ee9f71
cla6shade <cla6shade@gmail.com>
2025-12-15 16:52:24 +0900

    design: vertical padding 조정

--- src/features/blog/controller/PostController.tsx
+++ src/features/blog/controller/PostController.tsx
@@ -10,7 +10,7 @@ export default function PostController() {
 
   return (
     <Flex className="w-60 fixed px-2">
-      <Flex direction="col" className="w-full px-4 py-2 border border-border rounded-md gap-1">
+      <Flex direction="col" className="w-full px-4 py-4 border border-border rounded-md gap-1">
         {controllerItems.map((item, index) => {
           const isFocused = currentFocused?.href === item.href;
           const paddingClass = item.level === 1 ? 'pl-0' : item.level === 2 ? 'pl-3' : 'pl-6';
