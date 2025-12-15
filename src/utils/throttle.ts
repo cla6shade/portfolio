a77f4e1ca32a6395f410c4f877804bdfc01874f9
cla6shade <cla6shade@gmail.com>
2025-12-15 16:50:23 +0900

    fix: 잘못된 파라미터 명 수정

--- src/utils/throttle.ts
+++ src/utils/throttle.ts
@@ -1,13 +1,13 @@
 export default function throttle<T extends (...args: unknown[]) => void>(
   func: T,
-  debounceMs: number,
+  throttleMs: number,
 ): (...args: Parameters<T>) => void {
   let time: number | null = null;
 
   return function (...args: Parameters<T>) {
     const now = Date.now();
     if (time !== null) {
-      if (now - time < debounceMs) return;
+      if (now - time < throttleMs) return;
     }
     time = now;
     func(...args);
