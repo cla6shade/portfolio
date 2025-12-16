c3bbc3e7adb104b82d34aba7ef07de838f53834a
cla6shade <cla6shade@github.com>
2025-12-16 19:34:21 +0900

    chore: blogCard에서 필요없는 배경색 제거

--- src/features/blog/BlogPostCard.tsx
+++ src/features/blog/BlogPostCard.tsx
@@ -5,7 +5,7 @@ import { cva, type VariantProps } from 'class-variance-authority';
 import { Badge } from '@/components/ui/badge';
 import { cn } from '@/lib/utils';
 
-const cardVariants = cva('block group bg-background transition-all duration-300 overflow-hidden', {
+const cardVariants = cva('block group transition-all duration-300 overflow-hidden', {
   variants: {
     variant: {
       vertical: '',
