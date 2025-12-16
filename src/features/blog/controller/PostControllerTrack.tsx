8c2e8520b5798eb46a84edeeb0d6d21936ac5e68
cla6shade <cla6shade@github.com>
2025-12-16 19:16:25 +0900

    design: 컨트롤러가 태블릿 미만의 기기에서 보이지 않도록 수정

--- src/features/blog/controller/PostControllerTrack.tsx
+++ src/features/blog/controller/PostControllerTrack.tsx
@@ -3,7 +3,7 @@ import Flex from '@/components/container/Flex';
 
 export default function PostControllerTrack({ children }: { children: ReactNode }) {
   return (
-    <div className="py-32 w-60 h-full">
+    <div className="py-32 w-60 h-full hidden tablet:block">
       <Flex className="w-60 fixed px-4">{children}</Flex>
     </div>
   );
